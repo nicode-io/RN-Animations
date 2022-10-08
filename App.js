3660fa24eb44b77b9b6c16e24d410fbe78ef2047
Nicolas Denoël <nicolas.denoel@domintell.com>
2022-10-08 09:46:58 +0200

    Init App (Expo) and Worklets runOnUI runOnJS

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import {Worklets} from "./src/components/Worklets";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Worklets />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
